5554f042dd1d589b0c0f30b40eabfb88f4eb5e6f
Developer <7manakraj@gmail.com>
2026-05-08 15:57:37 +0530

    PWA: Explicitly register service worker for faster installation recognition.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './style.css';
+import { registerSW } from 'virtual:pwa-register';
+
+// Auto-register service worker
+registerSW({ immediate: true });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
